b61386e4e22c2b6960836b335c4eda51803fa0a1
daniel cheang <daniel@cpttm.org.mo>
2024-07-04 09:08:36 +0800

    show validation errors

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -5,15 +5,31 @@
 
   {{$url = empty($project->id) ? route('todos.store') : route('projects.todos.store', $project) }}
 
+  @if ($errors->any())
+    <div class="my-4 text-red-500">
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
+
   <form action="{{$url}}" method="post" class="mt-4 p-4">
       @csrf
       <div class="form-group m-3">
           <label for="name" class="block">Todo Name</label>
-          <input type="text" class="form-control" name="name">
+          <input type="text" name="name" class="@error('name') border-red-500 @enderror">
+          @error('name')
+            <div class="text-red-500">{{ $message }}</div>
+          @enderror
       </div>
       <div class="form-group m-3">
           <label for="description" class="block">Todo Description</label>
-          <textarea class="w-64" name="description" rows="3"></textarea>
+          <textarea name="description" rows="3" class="@error('name') border-red-500 @enderror"></textarea>
+          @error('description')
+            <div class="text-red-500">{{ $message }}</div>
+          @enderror
       </div>
       <div class="form-group m-3">
           <input type="submit" class="btn btn-primary" value="Submit">
