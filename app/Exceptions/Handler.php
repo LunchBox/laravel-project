b09b9b2cae4685bee138f452c9d5b78cd4a06598
daniel cheang <daniel@cpttm.org.mo>
2024-07-05 09:01:51 +0800

    handle error and customize error messages

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,26 +5,46 @@ namespace App\Exceptions;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Auth\AuthenticationException;
+
+use Illuminate\Http\Request;
+
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+  /**
+   * The list of the inputs that are never flashed to the session on validation exceptions.
+   *
+   * @var array<int, string>
+   */
+  protected $dontFlash = [
+    'current_password',
+    'password',
+    'password_confirmation',
+  ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-    }
+  /**
+   * Register the exception handling callbacks for the application.
+   */
+  public function register(): void
+  {
+    $this->reportable(function (Throwable $e) {
+      //
+    });
+
+    $this->renderable(function (AuthenticationException $e, Request $request) {
+      // if ($request->expectsJson()) {
+      if( $request->is('api/*')){
+        return response()->json(['message' => 'Please Login!'], 401);
+      }
+    });
+
+    $this->renderable(function (NotFoundHttpException $e, Request $request) {
+      // if ($request->expectsJson()) {
+      if( $request->is('api/*')){
+        return response()->json(['message' => 'Not Found!'], 404);
+      }
+    });
+  }
 }
